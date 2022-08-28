a835fea50a251b88f64123d744a9d3781b9f4373
Lizansh MAthur <lizanshmathur611@gmail.com>
2022-08-28 21:48:22 +0530

    Aima

--- index.js
+++ index.js
@@ -9,95 +9,40 @@ var para  = ["My friends and I are crazy. That’s the only thing that keeps us 
     var paragrap = para[i];
     $("p").html(paragrap);
 
-
-    
-
-
     $(document).ready(function(){
-      
-
-        $(".btn-change").click(function(){
-        if($("body").hasClass("body-light")){
-                changeTheme();
-        }
-        else{
-            changeThemeAgain();
-        }
-});
-        
-            $("#btn-demon").click(function(){
-                    var str = $("#demon-id").val();
-                   let result = findDemonName(str);
-                   $("#demon-id").val('')
-                    $(".answer-dark").html("Your Demon Name is: "+ result);           
-            });
-                $(".btn-s").click(function(){
-                    var str = $("#demon-id").val();
-                    let result = findFunnyName(str);
-                    $("#demon-id").val('');
-                       $(".answer").html("Your Funny Name is: "+ result);
-                });
-                
-                window.onbeforeunload = function() {
-                    if($("body").hasClass("body-dark")){
-                         return "After this you will be return to 'Funny Name 😂'";
-                    }
-                  }
+       
+       
+            $(".btn-s").click(function(){ 
+                var str = $("#demon-id").val();
+                let result = findFunnyName(str);
+                $("#demon-id").val('');
+                $(".answer").html("Your funny Name is: "+ result);
+                 });
+           
+            
     });
    
-    function changeTheme(){
-        $("#favicon").attr("href","darkfav.ico");
-        $("title").text("Demon Name 😈");
-        $("body").removeClass("body-light");    
-        $("body").addClass("body-dark");
-        $("h1").addClass("h1-dark").text("😈 Demon Name 😈");
-        $("h4").addClass("h4-dark").text("Let's find your Demon name");
-        $(".answer").addClass("answer-dark").text("Your Demon Name is: ");
-        $(".answer").removeClass("answer");
-        $("p").addClass("p-dark");
-        $(".btn-change").removeClass("btn-dark").addClass("btn-light").text("Funny Name 😂");
-        $(".btn-s").text("Let's See 😈").addClass("btn-s-dark").removeClass("btn-s");
-
-    }
-    function changeThemeAgain(){  
-        $("#favicon").attr("href","fav.ico");   
-        $("title").text("Funny Name 😂");   
-        $("body").removeClass("body-dark");    
-        $("body").addClass("body-light");
-        $("h1").removeClass("h1-dark").text("😂 Funny Name 😂");
-        $("h4").removeClass("h4-dark").text("Let's find your funny name");;
-        $(".answer-dark").addClass("answer").text("Your funny Name is: ");
-        $(".answer-dark").removeClass("answer-dark");
-        $("p").removeClass("p-dark");
-        $(".btn-change").removeClass("btn-light").addClass("btn-dark").text("Demon Name 😈");
-        $(".btn-s-dark").text("Let's See 😂").addClass("btn-s").removeClass("btn-s-dark");
-    }
-
+      
    
-    function findDemonName(str){
-            var output ="";
-            for(let i=str.length-1;i>=0;i--){
-                    output += str[i];
-            }
-            return output;
-    }
+               
+   
+    
 
     function findFunnyName(str) {
-        //let arr = str.split('');//4 
-        var sub = str.substring(0,(Math.floor(str.length/2)));
-       //console.log(sub);
-        let alfa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',   
-                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];//26
-        let len = Math.floor((str.length/2));
-        var funstr ="";
-        for (let i = 0; i <= len; i++) {
-            
-            j = Math.floor(Math.random() * 26);
-            funstr = funstr +alfa[j];
-             // console.log(funstr);
+  
+        let alfa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];//26
+        let len = Math.floor(str.length / 2); //2
+        let looplength = len;
+        for (let i = 0; i <= looplength; i++) {
+      
+          j = Math.floor(Math.random() * 26);
+           str = str.replace(str[len],alfa[j]);
+            len++;
         }
-          
-        var  res = sub+funstr;
-          //console.log(res);
-        return res;
-      }  
+      
+        return str;
+      
+      }
+
+      
